079a4870ee01076aeca27aa3f24210e5ad85f6a6
george kettle <georgekettle.gk@gmail.com>
2025-03-28 15:46:17 -0700

    shadcn-svelte styles working

--- app/frontend/pages/Home.svelte
+++ app/frontend/pages/Home.svelte
@@ -5,8 +5,10 @@
   <title>Home</title>
 </svelte:head>
 
-<div>
-  <h1>Home</h1>
+<div class="max-w-7xl mx-auto p-4">
+  <div class="bg-card text-card-foreground rounded-lg border shadow-sm p-6">
+    <h1 class="font-bold text-xl">Home</h1>
+  </div>
 </div>
 
 <style>
